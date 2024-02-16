4313ff64c3e41868400169df557fd23b227cd75d
Ahmad Alfuqaha <148557928+AhmadAlfuqaha@users.noreply.github.com>
2024-02-16 13:51:55 +0300

    Update Mux3to_1_d.v

--- SingleCycle/Mux3to_1_d.v
+++ SingleCycle/Mux3to_1_d.v
@@ -1,21 +1,3 @@
-/*module Mux3to_1_d(x,y,z,S,out);
-		input [31:0]x,y,z;
-		input [1:0]S;
-		output [31:0] out;
-		reg [31:0] out;
-		
-		
-		always@(*)begin
-		case(S)
-		2'b00: out = x;
-		2'b01: out = y;
-		2'b10: out = z;
-		endcase
-		end 
-		
-		
-endmodule*/
-
 module Mux3to_1_d(x,y,z,S,out);
 		parameter n = 32 ;
 		input [n-1:0]x,y,z;
@@ -24,7 +6,7 @@ module Mux3to_1_d(x,y,z,S,out);
 		
 		
 		assign out = (S == 2'b00) ? x : (S == 2'b01) ? y : (S == 2'b10) ? z : 0 ;
-		// i coded like this because the RTL viewer shows latches 
+		
 		
 		
 endmodule
